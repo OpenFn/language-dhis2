45c8e135531b82958a1cba0f1c4a509c6a517345
Chaiwa Berian <Chaiwa Berian>
2020-12-15 13:30:53 +0200

    Update, getSchema util, for #3

--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,29 @@
 import { eq, filter, some, isEmpty } from 'lodash';
-import { req } from './Client';
+import axios from 'axios';
+import { expandReferences } from 'language-common';
+
+axios.interceptors.response.use(
+  function (response) {
+    Log.info(`Response headers ${prettyJson(response.headers)}`);
+    // if (
+    //   response.headers['content-type']?.split(';')[0] === 'application/json' &&
+    //   (parseInt(response.headers['content-length']) ?? 0) > 0
+    // ) {
+    //   return { ...response, data: JSON.parse(response.data) };
+    // }
+    return response;
+  },
+  function (error) {
+    Log.error(`${error?.message}`);
+    return Promise.reject({
+      status: error?.response?.status,
+      message: error?.message,
+      url: error?.response?.config?.url,
+      responseData: error?.response?.data,
+      isAxiosError: error?.isAxiosError,
+    });
+  }
+);
 
 export function getApiResources(
   url,
@@ -33,126 +57,265 @@ export function getApiResources(
 
   warnExpectLargeResult(params, url);
 
-  return req({ url, auth, responseType, transformResponse });
+  return axios
+    .request({ url, auth, responseType, transformResponse })
+    .then(result => result);
 }
 
 /**
- * TODO
  *
- * Create a DHIS2 UID-Useful for client generated Ids compatible with DHIS2
+ * @param {string} resourceType
+ * @param {*} params
+ * @param {*} options
+ * @param {*} callback
  */
-export function generateUID() {
-  return;
+export function getSchema(resourceType, params, options, callback) {
+  return state => {
+    const { username, password, hostUrl, apiVersion } = state.configuration;
+
+    const queryParams = expandReferences(params)(state);
+
+    const useApiVersion = options?.supportApiVersion;
+
+    const url = buildUrl(
+      `/schemas/${resourceType}`,
+      hostUrl,
+      apiVersion,
+      useApiVersion
+    );
+
+    logApiVersion(state.configuration, options);
+
+    logWaitingForServer(url, queryParams);
+
+    warnExpectLargeResult(resourceType, url);
+
+    return axios
+      .request({
+        method: 'GET',
+        url,
+        auth: {
+          username,
+          password,
+        },
+        params: queryParams,
+      })
+      .then(result => {
+        Log.info(
+          composeSuccessMessage(
+            'getSchema',
+            resourceType,
+            queryParams,
+            options,
+            callback
+          )
+        );
+        return handleResponse(result, state);
+      });
+  };
 }
 
 /**
- * TODO
  *
- * Validate if valid DHIS2 UID
+ * @param {*} resourceType
+ * @param {*} params
+ * @param {*} options
  * @example
- * isValidUID('MmwcGkxy876')
- * // true
- * isValidUID(12345)
- * // false
- *
+ * getData(
+ * 'trackedEntityInstances',
+ * {
+ *  fields: '*',
+    ou: 'DiszpKrYNg8',
+    entityType: 'nEenWmSyUEp',
+    // filter: 'id:eq:FQ2o8UBlcrS',
+    skipPaging: true,
+    async: true,
+    // filter: 'id:eq:PWxgadk4sCG',
+  },
+  {
+    includeSystem: false,
+  }
+);
  */
-export function isValidUID(target) {
-  return true | false;
+export function getData(resourceType, params, options, callback) {
+  return state => {
+    const { username, password } = state.configuration;
+
+    const queryParams = expandReferences(params)(state);
+
+    const url = buildUrl(getData, resourceType, state.configuration, options);
+
+    logApiVersion(state.configuration, options);
+
+    logWaitingForServer(url, queryParams);
+
+    warnExpectLargeResult(resourceType, url);
+
+    return axios
+      .request({
+        method: 'GET',
+        url,
+        auth: {
+          username,
+          password,
+        },
+        params: queryParams,
+      })
+      .then(result => {
+        Log.info(
+          composeSuccessMessage(
+            getData,
+            resourceType,
+            queryParams,
+            options,
+            callback
+          )
+        );
+
+        if (callback) return callback(composeNextState(state, result));
+
+        return composeNextState(state, result);
+      });
+  };
 }
 
 /**
- * Print easy-readable JSON objects, uses JSON.stringify.
- *
+ * Get metadata
+ * @param {*} resources 
+ * @param {*} params 
+ * @param {*} options 
+ * @param {*} callback 
+ * @example
+ * getMetadata(
+  'attributes',
+  {
+    fields: '*',
+    // filter: 'id:eq:PWxgadk4sCG',
+  },
+  {
+    includeSystem: false,
+  }
+);
  */
+export function getMetadata(resources, params, options, callback) {
+  return state => {
+    const { username, password } = state.configuration;
 
-export function prettyJson(data) {
-  return JSON.stringify(data, null, 2);
+    const queryParams = expandReferences({ ...resources, ...params })(state);
+
+    const url = buildUrl(getMetadata, resources, state.configuration, options);
+
+    logApiVersion(state.configuration, options);
+
+    logWaitingForServer(url, queryParams);
+
+    warnExpectLargeResult(resources, url);
+
+    return axios
+      .request({
+        method: 'GET',
+        url,
+        auth: {
+          username,
+          password,
+        },
+        params: queryParams,
+      })
+      .then(result => {
+        Log.info(
+          composeSuccessMessage(
+            getMetadata,
+            resources,
+            queryParams,
+            options,
+            callback
+          )
+        );
+
+        if (callback) return callback(composeNextState(state, result));
+
+        return composeNextState(state, result);
+      });
+  };
 }
 
-/**
- * Custom logger with timestamps and colors
- *
- */
-export class Log {
-  static #OPTIONS = {
-    INFO: 'INFO',
-    WARN: 'WARN',
-    ERROR: 'ERROR',
+export function postData(resourceType, data, params, options, callback) {
+  return state => {
+    const { username, password } = state.configuration;
+
+    const queryParams = expandReferences(params)(state);
+
+    const payload = expandReferences(data);
+
+    const url = buildUrl(postData, resourceType, state.configuration, options);
+
+    logApiVersion(state.configuration, options);
+
+    logWaitingForServer(url, queryParams);
+
+    warnExpectLargeResult(resourceType, url);
+
+    return axios
+      .request({
+        method: 'POST',
+        url,
+        auth: {
+          username,
+          password,
+        },
+        params: queryParams,
+        data: payload,
+      })
+      .then(result => {
+        Log.info(
+          composeSuccessMessage(
+            getMetadata,
+            resources,
+            queryParams,
+            options,
+            callback
+          )
+        );
+
+        if (callback) return callback(composeNextState(state, result));
+
+        return composeNextState(state, result);
+      });
   };
-  /*
-  * For example, \x1b[31m is an escape sequence that will be intercepted by the terminal 
-    and instructs it to switch to the red color. 
-    In fact, \x1b is the code for the non-printable control character escape. 
-    Escape sequences dealing only with colors and styles are also known as ANSI escape code(https://en.wikipedia.org/wiki/ANSI_escape_code#Colors) 
-    and are standardized, so therefore they (should) work on any platform
-  */
-  static #COLORS = {
-    Reset: '\x1b[0m',
-    Bright: '\x1b[1m',
-    Dim: '\x1b[2m',
-    Underscore: '\x1b[4m',
-    Blink: '\x1b[5m',
-    Reverse: '\x1b[7m',
-    Hidden: '\x1b[8m',
+}
+export function postMetadata(resourceType, data, params, options) {
+  return state;
+}
 
-    FgBlack: '\x1b[30m',
-    FgRed: '\x1b[31m',
-    FgGreen: '\x1b[32m',
-    FgYellow: '\x1b[33m',
-    FgBlue: '\x1b[34m',
-    FgMagenta: '\x1b[35m',
-    FgCyan: '\x1b[36m',
-    FgWhite: '\x1b[37m',
-
-    BgBlack: '\x1b[40m',
-    BgRed: '\x1b[41m',
-    BgGreen: '\x1b[42m',
-    BgYellow: '\x1b[43m',
-    BgBlue: '\x1b[44m',
-    BgMagenta: '\x1b[45m',
-    BgCyan: '\x1b[46m',
-    BgWhite: '\x1b[47m',
-  };
-
-  static #MAIN_TAG = 'openfn';
-
-  static #printMessage(prefix, message) {
-    switch (prefix) {
-      case Log.#OPTIONS.WARN:
-        console.warn(
-          `${Log.#MAIN_TAG} ${
-            Log.#COLORS.FgYellow
-          }%s\x1b[0m ${new Date()}\n ${message}`,
-          Log.#OPTIONS.WARN
-        );
-        break;
-      case Log.#OPTIONS.ERROR:
-        console.error(
-          `${Log.#MAIN_TAG} ${
-            Log.#COLORS.FgRed
-          }%s\x1b[0m ${new Date()}\n ${message}`,
-          Log.#OPTIONS.ERROR
-        );
-        break;
-      default:
-        console.info(
-          `${Log.#MAIN_TAG} ${
-            Log.#COLORS.FgGreen
-          }%s\x1b[0m ${new Date()}\n ${message}`,
-          Log.#OPTIONS.INFO
-        );
-    }
-  }
-
-  static info(message) {
-    return Log.#printMessage(Log.#OPTIONS.INFO, message);
-  }
-  static warn(message) {
-    return Log.#printMessage(Log.#OPTIONS.WARN, message);
-  }
-  static error(message) {
-    return Log.#printMessage(Log.#OPTIONS.ERROR, message);
-  }
+export function upsertData(
+  resourceType,
+  uniqueAttribute,
+  data,
+  params,
+  options
+) {
+  return state;
+}
+export function upsertMetadata(
+  resourceType,
+  uniqueAttribute,
+  data,
+  params,
+  options
+) {
+  return state;
+}
+export function updateData(resourceType, query, data, params, options) {
+  return state;
+}
+export function updateMetadata(resourceType, query, data, params, options) {
+  return state;
+}
+export function deleteData(resourceType, query, params, options) {
+  return state;
+}
+export function deleteMetadata(resourceType, query, params, options) {
+  return state;
 }
 
 /**
@@ -275,6 +438,124 @@ export function logWaitingForServer(url, params) {
   Log.info(`url ${url}`);
   Log.info(`params ${prettyJson(params)}`);
   Log.info(`Waiting for server response on ${url} ...`);
+}
+
+/**
+ * TODO
+ *
+ * Create a DHIS2 UID-Useful for client generated Ids compatible with DHIS2
+ */
+export function generateUID() {
+  return;
+}
+
+/**
+ * TODO
+ *
+ * Validate if valid DHIS2 UID
+ * @example
+ * isValidUID('MmwcGkxy876')
+ * // true
+ * isValidUID(12345)
+ * // false
+ *
+ */
+export function isValidUID(target) {
+  return true | false;
+}
+
+/**
+ * Print easy-readable JSON objects, uses JSON.stringify.
+ *
+ */
+export function prettyJson(data) {
+  return JSON.stringify(data, null, 2);
+}
+
+/**
+ * Custom logger with timestamps and colors
+ *
+ */
+export class Log {
+  static #OPTIONS = {
+    INFO: 'INFO',
+    WARN: 'WARN',
+    ERROR: 'ERROR',
+  };
+  /*
+  * For example, \x1b[31m is an escape sequence that will be intercepted by the terminal 
+    and instructs it to switch to the red color. 
+    In fact, \x1b is the code for the non-printable control character escape. 
+    Escape sequences dealing only with colors and styles are also known as ANSI escape code(https://en.wikipedia.org/wiki/ANSI_escape_code#Colors) 
+    and are standardized, so therefore they (should) work on any platform
+  */
+  static #COLORS = {
+    Reset: '\x1b[0m',
+    Bright: '\x1b[1m',
+    Dim: '\x1b[2m',
+    Underscore: '\x1b[4m',
+    Blink: '\x1b[5m',
+    Reverse: '\x1b[7m',
+    Hidden: '\x1b[8m',
+
+    FgBlack: '\x1b[30m',
+    FgRed: '\x1b[31m',
+    FgGreen: '\x1b[32m',
+    FgYellow: '\x1b[33m',
+    FgBlue: '\x1b[34m',
+    FgMagenta: '\x1b[35m',
+    FgCyan: '\x1b[36m',
+    FgWhite: '\x1b[37m',
+
+    BgBlack: '\x1b[40m',
+    BgRed: '\x1b[41m',
+    BgGreen: '\x1b[42m',
+    BgYellow: '\x1b[43m',
+    BgBlue: '\x1b[44m',
+    BgMagenta: '\x1b[45m',
+    BgCyan: '\x1b[46m',
+    BgWhite: '\x1b[47m',
+  };
+
+  static #MAIN_TAG = 'openfn';
+
+  static #printMessage(prefix, message) {
+    switch (prefix) {
+      case Log.#OPTIONS.WARN:
+        console.warn(
+          `${Log.#MAIN_TAG} ${
+            Log.#COLORS.FgYellow
+          }%s\x1b[0m ${new Date()}\n ${message}`,
+          Log.#OPTIONS.WARN
+        );
+        break;
+      case Log.#OPTIONS.ERROR:
+        console.error(
+          `${Log.#MAIN_TAG} ${
+            Log.#COLORS.FgRed
+          }%s\x1b[0m ${new Date()}\n ${message}`,
+          Log.#OPTIONS.ERROR
+        );
+        break;
+      default:
+        console.info(
+          `${Log.#MAIN_TAG} ${
+            Log.#COLORS.FgGreen
+          }%s\x1b[0m ${new Date()}\n ${message}`,
+          Log.#OPTIONS.INFO
+        );
+    }
+  }
+
+  static info(message) {
+    return Log.#printMessage(Log.#OPTIONS.INFO, message);
+  }
+  static warn(message) {
+    return Log.#printMessage(Log.#OPTIONS.WARN, message);
+  }
+  static error(message) {
+    return Log.#printMessage(Log.#OPTIONS.ERROR, message);
+  }
 }
 
 /**
@@ -681,37 +962,6 @@ export const FIELD_TRANSFORMERS = [
 /** 
  * OTHER NOTES
  * 
- * 1.10.5. Validating payloads
-System wide validation of metadata payloads are enabled from 2.19 release, this means that create/update operations on the web-api endpoints will be checked for valid payload before allowed changes to be made, to find out what validations are in place for a endpoint, please have a look at the /api/schemas endpoint, i.e. to figure out which constraints a data element have, you would go to /api/schemas/dataElement.
-
-You can also validate your payload manually by sending it to the proper schema endpoint. If you wanted to validate the constant from the create section before, you would send it like this:
-
-POST /api/schemas/constant
-{ payload }
-A simple (non-validating) example would be:
-
-curl -X POST -d "{\"name\": \"some name\"}" -H "Content-Type: application/json" 
--u admin:district https://play.dhis2.org/dev/api/schemas/dataElement
-Which would yield the result:
-
-[
-   {
-      "message" : "Required property missing.",
-      "property" : "type"
-   },
-   {
-      "property" : "aggregationOperator",
-      "message" : "Required property missing."
-   },
-   {
-      "property" : "domainType",
-      "message" : "Required property missing."
-   },
-   {
-      "property" : "shortName",
-      "message" : "Required property missing."
-   }
-]
 1.10.6. Partial updates
 For cases where you don't want or need to update all properties on a object (which means downloading a potentially huge payload, change one property, then upload again) we now support partial update, both for single properties and for multiple properties.
 
