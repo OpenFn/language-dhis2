4d44dde6018adc4acc7c85f98afbb827b3f8f90d
Elias W. BA <elias@openfn.org>
2021-11-17 02:47:13 +0000

    Done testing update

--- test/integration.js
+++ test/integration.js
@@ -34,11 +34,11 @@ const globalState = {
   dataSet: 'pBOMPrpg1QX',
   trackedEntityInstance: 'bmshzEacgxa',
   programStage: 'A03MvHHogjR',
+  dataElement: 'Ix2HsbDMLea',
+  enrollment: 'CmsHzercTBa',
 };
 
 describe('create', () => {
-  before('prepare data', async () => {});
-
   it('should create an event program', async () => {
     const state = {
       ...globalState,
@@ -74,6 +74,7 @@ describe('create', () => {
     const response = await execute(create('events', state => state.data))(
       state
     );
+    globalState.event = response.data.response.uid;
     expect({
       httpStatus: response.data.httpStatus,
       httpStatusCode: response.data.httpStatusCode,
@@ -99,10 +100,11 @@ describe('create', () => {
         ],
       },
     };
-
     const response = await execute(
       create('trackedEntityInstances', state => state.data)
     )(state);
+    globalState.trackedEntityInstance =
+      response.data.response.importSummaries[0].reference;
     expect({
       httpStatus: response.data.httpStatus,
       httpStatusCode: response.data.httpStatusCode,
@@ -128,7 +130,6 @@ describe('create', () => {
     const response = await execute(
       create('dataValueSets', state => state.data)
     )(state);
-    console.log(response);
     expect({ status: response.data.status }).to.eql({ status: 'SUCCESS' });
   });
 
@@ -160,7 +161,6 @@ describe('create', () => {
     const response = await execute(
       create('dataValueSets', state => state.data)
     )(state);
-    console.log(response);
     expect({ status: response.data.status }).to.eql({ status: 'SUCCESS' });
   });
 
@@ -179,7 +179,168 @@ describe('create', () => {
     const response = await execute(create('enrollments', state => state.data))(
       state
     );
-    console.log('RESPONSE', response);
     expect({ status: response.data.status }).to.eql({ status: 'SUCCESS' });
   });
-}).timeout(20000);
+});
+
+describe('update', () => {
+  it('should update an event program', async () => {
+    const state = {
+      ...globalState,
+      data: { program: getRandomProgramPayload() },
+    };
+
+    const response = await execute(
+      update(
+        'programs',
+        state => state.program,
+        state => state.data.program
+      )
+    )(state);
+    expect({
+      httpStatus: response.data.httpStatus,
+      httpStatusCode: response.data.httpStatusCode,
+      status: response.data.status,
+    }).to.eql({
+      httpStatus: 'OK',
+      httpStatusCode: 200,
+      status: 'OK',
+    });
+  });
+
+  it('should update a single event', async () => {
+    const state = {
+      ...globalState,
+      data: {
+        eventDate: new Date().toISOString().split('T')[0],
+        status: 'COMPLETED',
+      },
+    };
+    const response = await execute(
+      update(
+        'events',
+        state => state.event,
+        state => state.data
+      )
+    )(state);
+    expect({
+      httpStatus: response.data.httpStatus,
+      httpStatusCode: response.data.httpStatusCode,
+      status: response.data.status,
+    }).to.eql({
+      httpStatus: 'OK',
+      httpStatusCode: 200,
+      status: 'OK',
+    });
+  });
+
+  it('should update a single tracked entity instance', async () => {
+    const state = {
+      ...globalState,
+      data: {
+        orgUnit: globalState.organisationUnit,
+        trackedEntityType: 'nEenWmSyUEp',
+        attributes: [
+          {
+            attribute: 'w75KJ2mc4zz',
+            value: 'Gigiwe',
+          },
+        ],
+      },
+    };
+    console.log(state);
+    const response = await execute(
+      update(
+        'trackedEntityInstances',
+        state => state.trackedEntityInstance,
+        state => state.data
+      )
+    )(state);
+    expect({
+      httpStatus: response.data.httpStatus,
+      httpStatusCode: response.data.httpStatusCode,
+      status: response.data.status,
+    }).to.eql({
+      httpStatus: 'OK',
+      httpStatusCode: 200,
+      status: 'OK',
+    });
+  });
+
+  it('should update a single dataValueSet', async () => {
+    const state = {
+      ...globalState,
+      data: {
+        dataElement: 'f7n9E0hX8qk',
+        period: '201401',
+        orgUnit: globalState.organisationUnit,
+        value: '12',
+      },
+    };
+    const response = await execute(
+      update(
+        'dataValueSets',
+        state => state.dataSet,
+        state => state.data
+      )
+    )(state);
+    expect({ status: response.data.status }).to.eql({ status: 'SUCCESS' });
+  });
+
+  it('should update a set of related data values sharing the same period and organisation unit', async () => {
+    const state = {
+      ...globalState,
+      data: {
+        dataSet: globalState.dataSet,
+        completeDate: '2014-02-03',
+        period: '201401',
+        orgUnit: globalState.organisationUnit,
+        dataValues: [
+          {
+            dataElement: 'f7n9E0hX8qk',
+            value: '1',
+          },
+          {
+            dataElement: 'Ix2HsbDMLea',
+            value: '2',
+          },
+          {
+            dataElement: 'eY5ehpbEsB7',
+            value: '3',
+          },
+        ],
+      },
+    };
+
+    const response = await execute(
+      update(
+        'dataValueSets',
+        state => state.dataSet,
+        state => state.data
+      )
+    )(state);
+    expect({ status: response.data.status }).to.eql({ status: 'SUCCESS' });
+  });
+
+  it('should update a single enrollment of a trackedEntityInstance into a given program', async () => {
+    const state = {
+      ...globalState,
+      data: {
+        trackedEntityInstance: globalState.trackedEntityInstance,
+        orgUnit: globalState.organisationUnit,
+        program: globalState.program,
+        enrollmentDate: new Date().toISOString().split('T')[0],
+        incidentDate: new Date().toISOString().split('T')[0],
+      },
+    };
+
+    const response = await execute(
+      update(
+        'enrollments',
+        state => state.enrollment,
+        state => state.data
+      )
+    )(state);
+    expect({ status: response.data.status }).to.eql({ status: 'SUCCESS' });
+  });
+});
